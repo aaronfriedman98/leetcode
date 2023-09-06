6ef65283cc36e0020dcfa3c542741be9316dedf0
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-06 13:36:05 -0400

    completed solution001

--- soluition001.js
+++ soluition001.js
@@ -1,1 +1,24 @@
-test
+// @problem - array, target -> indices of the 2 numbers that add to the target
+// @params - {number[]} nums, {number}, target
+// @return - {number[]}
+// @solution - use nested loops to add each of the numbers together 
+//             until we find the 2 numbers that add to the targer
+//             return the indices of those numbers
+
+function findIndices(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+      for(let j = 0; j < nums.length; j++) {
+        if(nums[i] + nums[j] === target) {
+          return [i,j]
+        }
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  console.log(findIndices([1,2,3,4,5], 9),' = [ 3, 4 ]')
+  console.log(findIndices([4,3,8,9], 12),' = [ 0 , 2 ]')
+  
