94e7736a475a00cb3ccd01886f33c82be9ff3163
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-08 12:15:15 -0400

    optimized solution

--- soluition001.js
+++ soluition001.js
@@ -5,6 +5,7 @@
 //             until we find the 2 numbers that add to the targer
 //             return the indices of those numbers
 
+// @time complexity - O(n^2)
 function findIndices(nums, target) {
     for(let i = 0; i < nums.length; i++) {
       for(let j = 0; j < nums.length; j++) {
@@ -14,6 +15,27 @@ function findIndices(nums, target) {
       }
     }
   }
+
+  //OPTIMIZED SOLUTION
+
+// @time complexity - O(n)
+  function findIndices(nums, target) {
+    //create Map object to store the nums and their indices
+    const numIndices = new Map()
+    //loop through nums and check if the complement num is in numIndices
+    for(let i = 0; i < nums.length; i++) {
+      let num = nums[i]
+      let complement = target - num
+  
+      if(numIndices.has(complement)) {
+        //return the index of the complement and return the current index
+        return [numIndices.get(complement), i]
+      }
+      numIndices.set(num, i)
+    }
+    return []
+  }
+  
   
   
   console.log(findIndices([1,2,3,4,5], 9),' = [ 3, 4 ]')
