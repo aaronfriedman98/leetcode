efe603c5290d3d6d374c68e4e281bfd49ccfa813
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-07 21:07:02 -0400

    solution001 updated

--- soluition001.js
+++ soluition001.js
@@ -8,15 +8,12 @@
 function findIndices(nums, target) {
     for(let i = 0; i < nums.length; i++) {
       for(let j = 0; j < nums.length; j++) {
-        if(nums[i] + nums[j] === target) {
+        if(nums[i] + nums[j] === target && i !== j) {
           return [i,j]
         }
       }
     }
   }
-  
-  
-  
   
   
   console.log(findIndices([1,2,3,4,5], 9),' = [ 3, 4 ]')
