b7ffcb00c1ff8a0113381220d7f1e7f2aa4a14a7
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-10 20:57:13 -0400

    solution002 completed

---
+++ solution002.js
@@ -0,0 +1,26 @@
+// @problem - create a function last() that can be used for any array and //            it will always returns the last element. If 
+//            there are no elements, return -1
+// @Params - none
+// @return - {last element}
+// @solution - simply return the last element of the array with
+//             arr.length - 1
+
+
+Array.prototype.last = function(){
+    if(this.length === 0) {
+      return -1
+    }
+    else {
+      return this[this.length - 1]
+    }
+  }
+  
+  
+  let arr1 = [1,2,3]
+  let arr2 = ['test', 'testing', 'tested']
+  let arr3 = []
+  
+  console.log(arr1.last()  + ' = 3')
+  console.log(arr2.last() + ' = tested')
+  console.log(arr3.last() + ' = -1')
+  
