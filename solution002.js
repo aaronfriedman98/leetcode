04720865997e018f6e47bde131f93a46025937c5
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-10 20:59:49 -0400

    ternary version

--- solution002.js
+++ solution002.js
@@ -5,15 +5,23 @@
 // @solution - simply return the last element of the array with
 //             arr.length - 1
 
+//Brute force solution
+// @time complexity - O(1)
+// Array.prototype.last = function(){
+//     if(this.length === 0) {
+//       return -1
+//     }
+//     else {
+//       return this[this.length - 1]
+//     }
+//   }
 
-Array.prototype.last = function(){
-    if(this.length === 0) {
-      return -1
-    }
-    else {
-      return this[this.length - 1]
-    }
-  }
+
+//es6 solution
+Array.prototype.last = function() {
+    return this.length === 0 ? -1 : this[this.length - 1]
+}
+
   
   
   let arr1 = [1,2,3]
