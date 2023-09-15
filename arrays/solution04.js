af490a7ad99c25800444249155d3356b3af98754
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-15 16:00:58 -0400

    optimized solution04

--- arrays/solution04.js
+++ arrays/solution04.js
@@ -2,6 +2,7 @@
 //params - {numbers[]} nums
 //return - {boolean}
 //solution - create another array, loop through the original array and copy each value to the new array, and in each iteration loop through the 2nd array and check for duplicate values
+//optimized solution - create a Map object, loop through the array and check if the Map has the current value, if it does then return true, if it doesn't then add the value to the Map and continue looping. If the loop finishes then return false
 
 function duplicates(nums) {
     let tempArray = []
@@ -13,6 +14,20 @@ function duplicates(nums) {
         }
       } tempArray.push(nums[i])
     } return false
+  }
+
+    //OPTIMIZED SOLUTION
+  function duplicates(nums) {
+    let numMap = new Map()
+    for(let i = 0; i < nums.length; i++) {
+      if(numMap.has(nums[i])) {
+        return true
+      }
+      else {
+        numMap.set(nums[i], i)
+      }
+    }
+    return false
   }
   
   console.log(duplicates([1,3,4,5,5,6,6])) //true
