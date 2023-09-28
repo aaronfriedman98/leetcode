35c4a90d9851d84eeecfe98eb25b6140e2fb0ee6
Aaron Friedman <aronfriedman98@gmail.com>
2023-09-28 13:54:50 -0400

    corrected solution for solution01

--- arrays/solution01.js
+++ arrays/solution01.js
@@ -8,15 +8,16 @@ function removeDuplicates(nums) {
     if(nums.length === 0) {
       return 0
     }
-    let uniqueNums = 1
-    for(let i = 1; i < nums.length; i++) {
-      if(nums[i] !== nums[i-1]) {
-        nums[uniqueNums] = nums[i]
-        uniqueNums++
+    let uniqueNumIndex = 1
+      for(let i = 1; i < nums.length; i++) {
+        if(nums[i] !== nums[i - 1] && nums[i] > nums[uniqueNumIndex - 1]) {
+          nums[uniqueNumIndex] = nums[i]
+          uniqueNumIndex++
+        }
       }
-    }
-    return uniqueNums
+      return uniqueNumIndex
   }
+
   
   
   
