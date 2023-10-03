670e5dbf1d716944175236bb8df9276d7cb4187e
Aaron Friedman <aronfriedman98@gmail.com>
2023-10-02 22:03:23 -0400

    commented out unnecessary line of code

--- arrays/solution01.js
+++ arrays/solution01.js
@@ -10,7 +10,9 @@ function removeDuplicates(nums) {
     }
     let uniqueNumIndex = 1
       for(let i = 1; i < nums.length; i++) {
-        if(nums[i] !== nums[i - 1] && nums[i] > nums[uniqueNumIndex - 1]) {
+        if(nums[i] !== nums[i - 1] 
+          // && nums[i] > nums[uniqueNumIndex - 1]
+          ) {
           nums[uniqueNumIndex] = nums[i]
           uniqueNumIndex++
         }
