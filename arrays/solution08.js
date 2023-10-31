38ca0b9a69c739180690db699f7b825ec5bbb975
Aaron Friedman <aronfriedman98@gmail.com>
2023-10-31 15:06:50 -0400

    added solution with comments

--- arrays/solution08.js
+++ arrays/solution08.js
@@ -1,7 +1,7 @@
 //description - given an array of nums, return the same array with all of the zeros moved to the end
 //params - {number[]} nums
 //return - {number[]}
-//solution - 2 pointer approach: create var that keeps track of the index's where the non-zero elements will go. Loop through the array and check if each element is equal to zero or not. If it is not equal to zero then assign the index with non-zero var to index i and assing index i to 0. Afterwards, add one to the non-zero variable. 
+//solution - 2 pointer approach: create var that keeps track of the index's where the non-zero elements will go. Loop through the array and check if each element is equal to zero or not. If it is not equal to zero then assign the index with non-zero var to index i and assign index i to 0 only if a number was swapped. Afterwards, add one to the non-zero variable. 
 
 
 function moveZeros(nums) {
@@ -18,6 +18,23 @@ function moveZeros(nums) {
     return nums
   }
   
+  //solution with comments
+  function moveZeros(nums) {
+    //initialize the nonZeroIndex to the beginning of the array
+    let nonZeroIndex = 0
+  
+    //loop through the array swapping zeros for numbers
+    for(let i = 0; i < nums.length; i++) {
+      if(nums[i] !== 0) {
+        nums[nonZeroIndex] = nums[i]
+        //if a number was swapped with a zero, then change the current index to a zero
+        if(i !== nonZeroIndex) {
+          nums[i] = 0
+        }
+        nonZeroIndex++
+      }
+    }
+  }
   
   console.log(moveZeros([1,0,4,0,3])) // [1,4,3,0,0]
   console.log(moveZeros([0,0,3,4,0,1])) // [3,4,1,0,0,0]
