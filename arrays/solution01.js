d7552af2e26626265511b4e3ca0b2bedc3f090cd
Aaron Friedman <aronfriedman98@gmail.com>
2023-11-15 22:24:04 -0500

    updated comments for solution01

--- arrays/solution01.js
+++ arrays/solution01.js
@@ -1,7 +1,7 @@
 //description - pass in an array of nums in ascending order, and remove all of the duplicates in place and return the number of unique elements
 //params - {number[]} nums
 //return - {number}
-//solution - create var for the unique numbers and set it to one since there is automatically one unique value. Loop through the array starting from the 2nd index since the first element is automatically a unique number. Compare the 2nd index of the array with the first index of the array, if they are the same then continue, if they are different, then first assign the value of i to nums[uniqueNumber] to make the element after the first unique number the next ascending number, and then add 1 to the unique number var. This will effectively place all of the unique numbers in the beginning of the array. Then return the unique number var.
+//solution - create var for the unique number index and set it to one since there is automatically one unique value. Loop through the array starting from the 2nd index since the first element is automatically a unique number. Compare the 2nd index of the array with the first index of the array, if they are the same then continue, if they are different, then first assign the value of nums[uniqueNumber] to nums[i], and then add 1 to the unique number var. This will effectively place all of the unique numbers in the beginning of the array. Then return the unique number var.
 
 
 function removeDuplicates(nums) {
