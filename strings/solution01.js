bce5c70dbea73d6237d2868a0e3b8b649ddd1882
Aaron Friedman <aronfriedman98@gmail.com>
2023-12-27 16:10:19 -0500

    updated comments and reflection

--- strings/solution01.js
+++ strings/solution01.js
@@ -1,9 +1,14 @@
-//description - reverse an array of strings, in place
-//params - {string[]} string
-//return - {string[]}
-//solution - 1. use the reverse array method. 2. loop through the array the length of the array / 2. create a temp variable, and each iteration swap the first with the last and etc.
+//description - write a function that takes in a string made up of an array of chars, and reverse it in-place
+//params - {char[]} s
+//return - void
+//solution #1 - use the built-in array.reverse method
+//solution #2 - brute force approach: loop through the array swapping elements in place:
+//Step 1: initialize var len to Math.floor(s.length / 2). Because we only need to iterate halfway down the array to successfully swap all of the elements.
+//Step 2: write the for loop that iterates from 0 up to len
+//Step 3: within the loop, initialize var temp and assign s[i] to it, then set s[i] to s[s.length - 1 - i], and then set s[s.length - 1 - i] to temp 
 
-//built in solution
+
+//optimal solution
 // function reverseString(string) {
 //   return string.reverse()
 // }
@@ -22,3 +27,7 @@ function reverseString(string) {
   
   console.log(reverseString(["o","l","e","h"])) // ["h","e","l","o"]
   console.log(reverseString(["o","l","l","e","h"])) // ["h","e","l","l","o"]
+
+
+//REFLECTION - Passed/failed  attempt 1
+//passed with the simple solution of using the reverse() array method. But failed first attempt to perform the reversal through brute force.
