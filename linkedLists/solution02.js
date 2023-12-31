1b568c98dd0c0dabba1e855337ce8a97eb1dc1cc
Aaron Friedman <aronfriedman98@gmail.com>
2023-12-30 23:52:05 -0500

    updated comments

--- linkedLists/solution02.js
+++ linkedLists/solution02.js
@@ -1,61 +1,82 @@
-//description - given a node head and an index n, return the node list with the nth node from the end removed
-//param - {ListNode} head, {number} n
+//description - given the head of a linked list and a number (n), remove the nth number from the end of the linked list, and return the head (linked list)
+//params - {ListNode} head, {number} n
 //return - {ListNode}
-//solution - create a dummy node that is placed before the head, create 2 variables called fast and slow that will be used to find the nth node from the end, use a loop to distance the fast variable from the slow variable nth nodes from each other, then use a while loop to move the fast and slow variables down the list until the end which will leave the slow.next as the node to be removed, return the head
+//solution - 2 pointer approach
+//Step 1: initialize var dummy, to act as a dummy node that comes before the head. This is in order to handle the edge case if the head node is removed.
+//Step 2: initialize vars - fast and slow, and assign the head to both of them. These will be used as pointers. slow will find the nth node from the end and fast will "tell" slow when to stop.
+//Step 3: distance slow and fast by nth nodes by only moving fast with a for loop.
+//Step 4: use a while loop to move fast and slow up by one until fast reaches null. This will leave slow with the location of the nth node from the end.
+//Step 5: remove the nth node from the end and return the head.
 
-function removeNthFromEnd(head, n) {
-    let dummy = new ListNode(0)
-    dummy.next = head
-    let fast = dummy
-    let slow = dummy
-  
-    for (let i = 0; i <= n; i++) {
-      fast = fast.next
-    }
-  
-    while (fast !== null) {
-      fast = fast.next
-      slow = slow.next
-    }
-  
-    slow.next = slow.next.next
-  
-    return head
+function deleteNthNodeFromEnd(head, n) {
+  //Create a dummy node and sets its next value to the head
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  //Initialize two pointers, fast and slow, both initially pointing to the dummy
+  let fast = dummy;
+  let slow = dummy;
+
+  //move the fast pointer ahead by n steps
+  for (let i = 0; i <= n; i++) {
+    fast = fast.next;
+  }
+
+  //move both pointers until fast reaches the end
+  while (fast !== null) {
+    fast = fast.next;
+    slow = slow.next;
   }
   
-  // test case
-  class ListNode {
-    constructor(val) {
-      this.val = val;
-      this.next = null;
+  //remove the nth node from the end
+  slow.next = slow.next.next;
+  //return the updated head
+  return dummy.next;
+}
+
+// Test Case with example
+
+//node constructor function
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+//linked list contructor function
+function LinkedList() {
+  this.head = null;
+
+  this.append = function (val) {
+    const newNode = new ListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
-  }
-  
-  // Helper function to convert a linked list to an array for easy testing
-  function linkedListToArray(head) {
-    const result = [];
-    let current = head;
-    while (current !== null) {
-      result.push(current.val);
+  };
+
+  this.print = function () {
+    current = this.head;
+    while (current) {
+      console.log(current.val);
       current = current.next;
     }
-    return result;
-  }
-  
-  // Test case
-  const node5 = new ListNode(5);
-  const node4 = new ListNode(4);
-  const node3 = new ListNode(3);
-  const node2 = new ListNode(2);
-  const node1 = new ListNode(1);
-  
-  node1.next = node2;
-  node2.next = node3;
-  node3.next = node4;
-  node4.next = node5;
-  
-  console.log(linkedListToArray(node1)); // Output: [1, 2, 3, 4, 5]
-  
-  const result = removeNthFromEnd(node1, 2);
-  console.log(linkedListToArray(result)); // Output: [1, 2, 3, 5]
-  
+  };
+}
+
+//test case
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+linkedList.print();
+
+const head = linkedList.head;
+console.log(deleteNthNodeFromEnd(head, 2)); //1
+
+linkedList.print(); // 1 > 2 > 4
