1ac6e061f421f446871423ea3d78279b5fda2aec
Aaron Friedman <aronfriedman98@gmail.com>
2024-01-02 14:56:03 -0500

    updated comments, code, and added new reflection

--- strings/solution02.js
+++ strings/solution02.js
@@ -3,34 +3,32 @@
 //return - {number}
 //solution - 
 //Step 1: initialize var isNegative, write an if statement that checks if the int is positive or negative and assign true or false to isNegative.
-//Step 2: initialize var reversedInt, assign it to - convert the int into a string, split the string into an array and reverse it, then join it, and afterwards parse it back into an integer.
+//Step 2: initialize var reversedInt, assign to it - convert the int into a string, split the string into an array and reverse it, then join it, and surround all this in a parseInt function to convert it back into an integer.
 //Step 3: if isNegative is true, make reversedInt negative
 //Step 4: check if the new integer is within the 32-bit range - initialize var upperbound and assign Math.pow(2,31) - 1 to it, and initialize var lowerbound and assign -upperbound to it.
 //Step 5: check if the int is within the range, if yes return the int, if not return 0.
 
 
-function reverse32bitInt(int) {
-  // check if int is positive or negative
-  let isNegative = int >= 0 ? false : true
-  // reversal - convert int > string > array > reverse > string > parseInt
-  let reversedInt = int.toString().split("").reverse().join("")
-  reversedInt = parseInt(reversedInt,10)
-  //remember if the int was negative and remake it negative if it was
-  if(isNegative) {
-    reversedInt = -reversedInt
-  }
-  //set the range for 32-bit integer
-  let upperbound = Math.pow(2, 31) - 1
+function reverseInt(num) {
+  //check if num is negative
+  let isNegative = false
+  num < 0 ? isNegative = true : isNegative
+  //reverse the number
+  let reversedInt = parseInt(num.toString().split("").reverse().join(""))
+  isNegative ? reversedInt = -reversedInt : reversedInt
+  //check if it is still in the 32-bit range
+  let upperbound = Math.pow(2,31)
   let lowerbound = -upperbound
-  //return the int if it is within the range, otherwise return 0
-  return reversedInt <= upperbound && reversedInt >=lowerbound ? reversedInt : 0
+  return reversedInt < upperbound && reversedInt > lowerbound ? reversedInt : 0
 }
 
 
-console.log(reverse32bitInt(123)) // 321
-console.log(reverse32bitInt(-123)) // -321
-console.log(reverse32bitInt(120)) // 21
-console.log(reverse32bitInt(-2345654323456765432)) // 0
+console.log(reverseInt(123)) // 321
+console.log(reverseInt(-123)) // -321
+console.log(reverseInt(120)) // 21
+console.log(reverseInt(-2345654323456765432)) // 0
 
 //REFLECTION - failed attempt 1
 //Wasnt sure how to deal with the 32-bit range. Also forgot to deal with negative edge case.
+//REFLECTION - failed attempt 2
+//remembered how to to reverse the number and how to handle 32-bit range. But forgot to deal with negative edge case.
