0b195a38b7c442c9713f8f9867d0038abe340e32
Aaron Friedman <aronfriedman98@gmail.com>
2024-01-05 16:07:43 -0500

    working on in-depth comments explaining the recursion

--- trees/solution01.js
+++ trees/solution01.js
@@ -1,7 +1,11 @@
-//description - given the root of a binary tree, return the max depth of the tree
+//description - given the root of a tree, return the max depth of the tree
 //params - {TreeNode} root
 //return - {number}
-//solution - use recursion to traverse all of the nodes in the tree and find the max depth
+//solution - use recursion to traverse the left and right nodes of the tree to find the max depth:
+//Step 1: set a base case so that the recursion will stop at some point - if(root === null) {return 0}
+//Step 2: initialize var leftDepth and set it recursively to maxDepth(root.left). What this does is it is recursively called until the param reaches past a leaf node and is equal to null. Once it equals null, the base case will stop the recursion and return 0. At this point, we are at the bottom of nested maxDepth function calls. leftDepth currently = 0.
+//Step 3: initialize var rightDepth and set it recursively to maxDepth(root.right). The same thing happens here and at the last function call it returns 0
+//Step 4: once these vars were assigned a value (0), return the maximum number between leftDepth and rightDepth and then add 1 to it. So, on the bottom most maxDepth function, leftDepth and rightDepth both = 0 of each of their respective depths. So, the maximum is automatically 0. And we add 1 to it so that we can count the node before we continue to unravel the recursion. In the next nested maxDepth function call, 
 
 
 
