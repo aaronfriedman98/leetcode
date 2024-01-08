94deacad72211bd1c5faf4750fb9bf0a060aca8c
Aaron Friedman <aronfriedman98@gmail.com>
2024-01-07 22:55:40 -0500

    updated comments, added reflection

--- sortingAndSearching/solution01.js
+++ sortingAndSearching/solution01.js
@@ -1,7 +1,12 @@
 //description - given 2 non-decreasing arrays of numbers and numbers representing the length of both arrays, merge the arrays as a non-decreasing array, but dont return a new array, rather modify the first array
-//params - {numbers[]} nums1, {number} len1, {numbers[]} nums2, {number} len2
-//return - {void} (modify nums1)
-//solution - use a 3 pointer approach: initialize p1 that will traverse nums1 and p2 that will traverse nums2 and p that will be used to place the values in the correct position of nums1, loop through the array as long as nums1 and nums2 still have values, in each iteration starting from the end of both arrays compare the values of each to find the larger value and copy that value into nums1[p] and then decrement whichever array had the largest value, once one of the arrays runs out of values and the while loop ends write another while loop that checks which array didnt run out and copys the rest of the elements into nums1
+//description - given 2 arrays of numbers in non-descending order - nums1 and nums2, and given 2 numbers, each representing the length of both of the arrays, merge the arrays together and have the merged array stored in nums1, without returning anything.
+//params - {number[]} nums1, {number[]} nums2, {number} n, {number} m
+//return - void
+//solution - 3 pointer approach
+//Step 1: initialize 3 var pointers - p, p1, p2. p = m + n - 1; p1 = n - 1; p2 = m - 1; (These pointers will act as different indices. p will be used to place the correct values into nums1. p1 will be used to traverse nums1 for comparison with nums2. p2 will be used to traverse nums2 for comparison with nums1.)
+//Step 2: write a while loop that runs as long as p1 !== 0. (We will be traversing the array backwards for this solution. In order to merge the array in-place we need to merge the array starting from the end, otherwise we could override nums1 elements when nums2 elements need to be placed before it. In order to solve it by traversing forward you would need to make a new array, which would not be merging it in-place)
+//Step 3: within each iteration, check if nums1[p1] <= nums2[p2]. If yes, then nums1[p] = nums2[p2]. Then p2--. If not, then nums1[p] = nums1[p1], and p1--.
+//Step 4: once either p1 or p2 === 0, the loop stops. If p2 ran out, then the arrays were successfully merged, since the remaining elements of nums1 are already in the correct place since we were given non-descending arrays. If p1 ran out, then we need one more while loop that copies the remaining values of nums2 into nums1.
 
 function mergeSortedArrays(nums1, m, nums2, n) {
     //initialize 3 pointers
@@ -34,3 +39,6 @@ function mergeSortedArrays(nums1, m, nums2, n) {
   let array1 = [1,3,5,7,10]
   mergeSortedArrays(array1, 5, [2,4,6,8], 4)
   console.log(array1) // [1,2,3,4,5,6,7,8,10]
+
+  //REFLECTION - failed attempt 1
+  //forgot the logic of using 3 pointer approach and traverse the arrays backwards
